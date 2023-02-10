0f7cfa50d72fe77724280bc24c886c333b8cf607
Caesar Klidge Galutera <caesarklidgegalutera@gmail.com>
2023-02-10 13:03:55 +0800

    login page create account skeleton

--- Project_galutera/resources/views/LoginPage/login.blade.php
+++ Project_galutera/resources/views/LoginPage/login.blade.php
@@ -4,5 +4,22 @@
 @endsection
 
 @section('content')
-    this is login page
+    <div class="loginPage">
+        <div class="loginPage--right">
+            <h1>Turn your dreams into reality</h1>
+            <p>Enroll Now!</p>
+        </div>
+        <div class="loginPage--left">
+            <h1>Create an account</h1>
+            <p>Enroll Now!</p>
+            <hr>
+            <input type="text" name="fname" placeholder="Firstname">
+            <input type="text" name="sname" placeholder="Surname">
+            <input type="text" name="email" placeholder="Email">
+            <input type="text" name="pword" placeholder="Password">
+            <button class="loginPage--left btn">Create account</button>
+
+            <p>Already have an account? <a href="#"> Log in</a></p>
+        </div>
+    </div>
 @endsection
