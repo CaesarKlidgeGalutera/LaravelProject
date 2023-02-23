2f408893b79490321ccdb8f29982613d508cf807
Caesar Klidge Galutera <caesarklidgegalutera@gmail.com>
2023-02-23 10:57:30 +0800

    Finished login LoginPage

--- Project_galutera/resources/views/LoginPage/login.blade.php
+++ Project_galutera/resources/views/LoginPage/login.blade.php
@@ -5,21 +5,32 @@
 
 @section('content')
     <div class="loginPage">
-        <div class="loginPage--right">
-            <h1>Turn your dreams into reality</h1>
-            <p>Enroll Now!</p>
-        </div>
         <div class="loginPage--left">
-            <h1>Create an account</h1>
-            <p>Enroll Now!</p>
-            <hr>
-            <input type="text" name="fname" placeholder="Firstname">
-            <input type="text" name="sname" placeholder="Surname">
-            <input type="text" name="email" placeholder="Email">
-            <input type="text" name="pword" placeholder="Password">
-            <button class="loginPage--left btn">Create account</button>
-
-            <p>Already have an account? <a href="#"> Log in</a></p>
+            <div class="loginPage--left__content">
+                <h1>Turn your dreams into reality</h1>
+                <p>Be the person you always dreamed of!</p>
+            </div>
+        </div>
+        <div class="loginPage--right">
+            <div class="loginPage--right__content">
+                <h2>Login</h2>
+                <p>Welcome back! Please enter your details.</p>
+                <div class="loginInfo">
+                    <input type="text" name="studendID" placeholder="Student Id">
+                    <input type="text" name="email" placeholder="Email">
+                    <input type="text" name="pword" placeholder="Password">
+                </div>
+                <div class="retain">
+                    <input type="checkbox" name="remember" id="rmmbr"> Remember me
+                    <a href="forgot">Forgot Password</a>
+                   
+                </div>
+                <button class="loginPage--left__btn">Log in</button>
+                <div class="Create">
+                    <p>Don't have an account? <a href="#"> Click here!</a> </p> 
+                    
+                </div>
+            </div>
         </div>
     </div>
 @endsection
